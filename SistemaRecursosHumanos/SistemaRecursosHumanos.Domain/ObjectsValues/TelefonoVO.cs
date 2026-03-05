f9686e0a78a239a2aa3de7780c5a10f20d017cc6
YsaiasWr <135547988+YsaiasWr@users.noreply.github.com>
2026-03-05 13:44:35 -0400

    Empleado implementado hasta Interfaces

---
+++ SistemaRecursosHumanos/SistemaRecursosHumanos.Domain/ObjectsValues/TelefonoVO.cs
@@ -0,0 +1,27 @@
+﻿using SistemaRecursosHumanos.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaRecursosHumanos.Domain.ObjectsValues
+{
+    
+        public sealed record TelefonoVO
+    {
+        public string Valor { get; }
+        public TelefonoVO(string valor)
+        {
+            if (string.IsNullOrWhiteSpace(valor))
+            {
+                throw new ExcepcionReglaNegocio("El Telefono no puede estar vacio");
+            }
+            else if (valor.Length != 10)
+            {
+                throw new ExcepcionReglaNegocio("El Documento debe tener exactamente 10 caracteres");
+            }
+            Valor = valor;
+        }
+    }
+}
---
+++ SistemaRecursosHumanos/SistemaRecursoshumanos.Application/Models/EmpleadoModel.cs
@@ -0,0 +1,33 @@
+﻿using SistemaRecursosHumanos.Domain.Entities;
+using SistemaRecursosHumanos.Domain.ObjectsValues;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaRecursoshumanos.Application.Models
+{
+    public class EmpleadoModel
+    {
+        public Guid? Idempleado { get; set; }
+        public string Tipoempleado { get; set; } = null!;
+        public DocumentoVO Documento { get; set; } = null!;
+        public string NombreCompleto { get; set; } = null!;
+        public TelefonoVO Telefono { get; set; } = null!;
+        public string Correo { get; set; } = null!;
+        public string Genero { get; set; } = null!;
+        public string Direccion { get; set; } = null!;
+        public string Estado { get; set; } = null!;
+        public Departamento oDepartamento { get; set; }
+        public Cargo oCargo { get; set; } = null!;
+        public DateTime FechaNacimiento { get; set; }
+        public string Cargo { get; set; } = null!;
+        public DateTime FechaIngreso { get; set; }
+        public decimal Salario { get; set; }
+        public string Horario { get; set; } = null!;
+        public string Horas { get; set; } = null!;
+        public byte[] Imagen { get; set; }
+
+    }
+}
